6017aeda2284ef72bf26b0b6d2cabcae8b522a06
SerzhikY <124130138+SerzhikY@users.noreply.github.com>
2023-04-10 18:19:54 +0300

    Add files via upload

---
+++ laba 8/laba 8/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_8
+{
+    abstract class postcard
+    {
+        public postcard(string n)
+        {
+            this.Name = n;
+        }
+        public string Name { get; protected set; }
+        public abstract string GetCost();
+    }
+
+    class Poorpostcard : postcard
+    {
+        public Poorpostcard() : base("Дешева листівка")
+        { }
+        public override string GetCost()
+        {
+            return "10";
+        }
+    }
+    class Expensifepostcard : postcard
+    {
+        public Expensifepostcard()
+            : base("Дорога листівка")
+        { }
+        public override string GetCost()
+        {
+            return "80";
+        }
+    }
+
+    abstract class postcardDecorator : postcard
+    {
+        protected postcard postcard;
+        public postcardDecorator(string n, postcard postcard) : base(n)
+        {
+            this.postcard = postcard;
+        }
+    }
+
+    class Goldenpostcard : postcardDecorator
+    {
+        public Goldenpostcard(postcard p)
+            : base(p.Name + ", з золотою рамкою", p)
+        { }
+
+        public override string GetCost()
+        {
+            return postcard.GetCost() + "30";
+        }
+    }
+
+    class Silverpostcard : postcardDecorator
+    {
+        public Silverpostcard(postcard p)
+            : base(p.Name + ", з срібною рамкою", p)
+        { }
+
+        public override string GetCost()
+        {
+            return postcard.GetCost() + "15";
+        }
+    }
+}
---
+++ laba 8/laba 8/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            postcard postcard1 = new Poorpostcard();
+            postcard1 = new Silverpostcard(postcard1); 
+            MessageBox.Show("Название: " + postcard1.Name);
+            MessageBox.Show("Цена: " + postcard1.GetCost());
+
+            postcard postcard2 = new Expensifepostcard();
+            postcard2 = new Goldenpostcard(postcard2);
+            MessageBox.Show("Название: " + postcard2.Name);
+            MessageBox.Show("Цена: " + postcard2.GetCost());
+
+            postcard postcard3 = new Expensifepostcard();
+            postcard3 = new Goldenpostcard(postcard3);
+            postcard3 = new Silverpostcard(postcard3);
+            MessageBox.Show("Название: " + postcard3.Name);
+            MessageBox.Show("Цена: " + postcard3.GetCost());
+        }
+    }
+}
